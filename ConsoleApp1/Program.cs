51db8fdacddfaa4fa6ad352ac5b32baccee8b82b
katyadess <trynovakatya345@gmail.com>
2025-05-16 14:43:17 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Random random = new Random();
+List<int> N = new List<int>();
+for (int i = 0; i < 10; i++)
+{
+    N.Add(random.Next(10, 100));
+    Console.Write("{0} ", N[i]);
+
+}
+
