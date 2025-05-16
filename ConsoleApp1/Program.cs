b8088fefd1fe9166473f4ad007c0c614d7b57280
katyadess <trynovakatya345@gmail.com>
2025-05-16 14:48:17 +0300

    add tasks 1 and 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,6 +9,47 @@ for (int i = 0; i < 10; i++)
     Console.Write("{0} ", N[i]);
 }
 
+
+//Task 1
+
+int minIndex = 0;
+for (int i = 0; i < N.Count; i++)
+{
+    if (N[i] < N[minIndex])
+    {
+        minIndex = i;
+    }
+}
+
+
+N.RemoveAt(minIndex);
+Console.WriteLine("\nКолекція після видалення першого мінімального значення:");
+Console.WriteLine(string.Join(" ", N));
+
+
+
+
+//Task 2
+
+int X = 5;
+for (int i = 0; i < N.Count; i++)
+{
+    if (N[i] % X == 0)
+    {
+        N.Insert(i, X);
+        i++;
+    }
+}
+
+
+Console.WriteLine($"\nКолекція після вставки числа {X} перед кожним числом, кратним {X}:");
+Console.WriteLine(string.Join(" ", N));
+
+
+
+
+//Task 3
+
 List<int> endsWithZero = new List<int>();
 List<int> others = new List<int>();
 
