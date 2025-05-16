852fe34d43d4b213d8004dc0a368c3d85eda891e
katyadess <trynovakatya345@gmail.com>
2025-05-16 14:45:09 +0300

    task 3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,26 @@ for (int i = 0; i < 10; i++)
 {
     N.Add(random.Next(10, 100));
     Console.Write("{0} ", N[i]);
-
 }
+
+List<int> endsWithZero = new List<int>();
+List<int> others = new List<int>();
+
+foreach (int number in N)
+{
+    if (number % 10 == 0)
+    {
+        endsWithZero.Add(number);
+    }
+    else
+    {
+        others.Add(number);
+    }
+}
+
+Console.WriteLine("\nЧисла, які закінчуються на 0:");
+Console.WriteLine(string.Join(" ", endsWithZero));
+
+Console.WriteLine("\nІнші числа:");
+Console.WriteLine(string.Join(" ", others));
 
